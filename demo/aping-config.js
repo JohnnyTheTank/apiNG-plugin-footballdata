df1527a26eedee36e4e78afc3cf7ad3cce9c8b62
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-06-24 19:10:15 +0200

    fixed demo

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -3,8 +3,7 @@ angular.module('jtt_aping').config(['$provide', function ($provide) {
     $provide.value("apingDefaultSettings", {
         apingApiKeys : {
             'footballdata': [
-                //{'api_key':'<YOUR_FOOTBALLDATA_API_KEY>'},
-                {'api_key':'e42cb6a6ecc949c8897e06d284a55e05'},
+                {'api_key':'<YOUR_FOOTBALLDATA_API_KEY>'},
             ],
             //...
         }
